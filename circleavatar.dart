5830fd6604516869a8e71c010b90f67b4a368caa
Sakshi Gangrade <130816650+23sakshigangrade@users.noreply.github.com>
2024-09-17 11:36:19 -0700

    Add files via upload

---
+++ circleavatar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Button'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Hello'),
+      ),
+      body:Center(
+
+        child:CircleAvatar(
+          child:Column(
+            children: [
+              Container(
+                  width: 30,
+              height: 30,
+                  child: Image.asset('assets/images/image2.jpg')),
+              Text('name')
+            ],
+
+          ),
+backgroundColor: Colors.green,
+ maxRadius: 50,
+            ),
+      )
+    );
+
+  }
+}
